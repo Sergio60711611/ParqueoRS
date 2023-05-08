86f02e67acd9771ac0aaa2ecefe4bfad91ecb31d
sergio60711611 <Sergio_01_gabriel@hotmail.com>
2023-05-07 22:08:40 -0400

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,11 +24,11 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-{
+    {
     if($this->app->environment('production')) {
         \URL::forceScheme('https');
     }
-}
+    }
 }
 
 
