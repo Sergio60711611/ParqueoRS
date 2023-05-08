771575c4c22a235fda7fa82c5f0ef5a8e08fefbc
sergio60711611 <Sergio_01_gabriel@hotmail.com>
2023-05-07 21:44:51 -0400

    login12

--- resources/views/inicio/login.blade.php
+++ resources/views/inicio/login.blade.php
@@ -3,7 +3,6 @@
 <head>
 	<title>Login</title>
 	<link rel="stylesheet" type="text/css" href="./css/menustyle.css">
-	href="./css/menustyle.css"
 	<link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
 	<script src="https://kit.fontawesome.com/a81368914c.js"></script>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
