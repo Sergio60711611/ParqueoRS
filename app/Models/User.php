618a95fe719302dfe3555c8fdb76709fcbb14549
melrodriguez1133 <202000231@est.umss.edu>
2023-05-07 20:58:34 -0400

    cambios

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
-
+//mel
     /**
      * The attributes that are mass assignable.
      *
