5a715b0b8f9c3655df3913bb4811e49e74a252ab
sergio60711611 <Sergio_01_gabriel@hotmail.com>
2023-05-07 21:10:23 -0400

    cambio css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link href="{{ asset('/css/menustyle.css') }}" media="all" rel="stylesheet" type="text/css">
+    <link href="./css/menustyle.css" media="all" rel="stylesheet" type="text/css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
