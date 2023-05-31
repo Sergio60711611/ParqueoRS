e7f7e1f05279d69f3099908b28c294266ea4b6a5
sergio60711611 <Sergio_01_gabriel@hotmail.com>
2023-05-31 16:54:54 -0400

    login implementacion

--- resources/views/inicio/login.blade.php
+++ resources/views/inicio/login.blade.php
@@ -1,46 +1,101 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-	<title>Login</title>
-	<link rel="stylesheet" type="text/css" href="https://parqueors-production.up.railway.app/css/loginstyle.css">
-	
-	<link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
-	<script src="https://kit.fontawesome.com/a81368914c.js"></script>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width-device-width,initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="sie-edge" />
+	<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+	<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+ 
+    <link rel="stylesheet" type="text/css" href="\css\loginstyle.css">
+    <title>RegistroInicio</title>
 </head>
+
 <body>
-	<img class="fondo" src="{{ asset('/img/autofondo.png') }}">
-	<div class="container">
-		<div class="img">
-			<img src="{{ asset('/img/mujerauto.svg') }}">
-		</div>
-		<div class="login-content">
-			<form action="/administrador/home">
-				<img src="{{ asset('/img/avatar.svg') }}">
-				<h2 class="title">Bienvenido</h2>
-           		<div class="input-div one">
-           		   <div class="i">
-           		   		<i class="fas fa-user"></i>
-           		   </div>
-           		   <div class="div">
-           		   		<h5>Usuario</h5>
-           		   		<input type="text" class="input">
-           		   </div>
-           		</div>
-           		<div class="input-div pass">
-           		   <div class="i"> 
-           		    	<i class="fas fa-lock"></i>
-           		   </div>
-           		   <div class="div">
-           		    	<h5>Contraseña</h5>
-           		    	<input type="password" class="input">
-            	   </div>
-            	</div>
-            	<a href="#">Olvidaste Tu Contraseña?</a>
-            	<input type="submit" class="btn" value="Login">
+
+    <div class="container" id="container">
+        <div class="form-container sign-up-container">
+					
+<form id="msform">
+  <!-- progressbar -->
+  <ul id="progressbar">
+    <li class="active">Account Setup</li>
+    <li>Social Profiles</li>
+    <li>Personal Details</li>
+  </ul>
+  <!-- fieldsets -->
+  <fieldset>
+    <h2 class="fs-title">Create your account</h2>
+    <h3 class="fs-subtitle">This is step 1</h3>
+    <input type="text" name="email" placeholder="Email" />
+    <input type="password" name="pass" placeholder="Password" />
+    <input type="password" name="cpass" placeholder="Confirm Password" />
+    <input type="button" name="next" class="next action-button" value="Next" />
+  </fieldset>
+  <fieldset>
+    <h2 class="fs-title">Social Profiles</h2>
+    <h3 class="fs-subtitle">Your presence on the social network</h3>
+    <input type="text" name="twitter" placeholder="Twitter" />
+    <input type="text" name="facebook" placeholder="Facebook" />
+    <input type="text" name="gplus" placeholder="Google Plus" />
+    <input type="button" name="previous" class="previous action-button" value="Previous" />
+    <input type="button" name="next" class="next action-button" value="Next" />
+  </fieldset>
+  <fieldset>
+    <h2 class="fs-title">Personal Details</h2>
+    <h3 class="fs-subtitle">We will never sell it</h3>
+    <input type="text" name="fname" placeholder="First Name" />
+    <input type="text" name="lname" placeholder="Last Name" />
+    <input type="text" name="phone" placeholder="Phone" />
+    <textarea name="address" placeholder="Address"></textarea>
+    <input type="button" name="previous" class="previous action-button" value="Previous" />
+    <a href="https://twitter.com/GoktepeAtakan" class="submit action-button" target="_top">Submit</a>
+  </fieldset>
+</form>
+            
+        </div>
+        <div class="form-container sign-in-container">
+            <form action="#">
+                <h1>Sign In</h1>
+                <div class="social-container">
+                    <a href="usuario" class="social" title="Usuario">
+						<ion-icon name="person-outline"></ion-icon>
+                    </a>
+                    <a href="#" class="social" title="Guardia">
+						<ion-icon name="walk-outline"></ion-icon>
+                    </a>
+                    <a href="#" class="social" title="Administrador">
+						<ion-icon name="person"></ion-icon>
+                    </a>
+                </div>
+                <span>or use your account</span>
+                <input type="email" placeholder="Email">
+                <input type="password" placeholder="Password">
+                <button>Sign In</button>
             </form>
         </div>
+        <div class="overlay-container">
+            <div class="overlay">
+                <div class="overlay-panel overlay-left">
+                    <h1>Welcome Back</h1>
+                    <p>To keep connected with us please login with your personal info</p>
+                    <button class="ghost" id="signIn">Sign In</button>
+					
+                </div>
+                <div class="overlay-panel overlay-right">
+                    <h1>Hello,Friend</h1>
+                    <p>Enter your personal details and start journey with us</p>
+                    <button class="ghost" id="signUp">Sign Up</button>
+                </div>
+            </div>
+        </div>
     </div>
-    <script type="text/javascript" src="https://parqueors-production.up.railway.app/js/login.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="\js\login.js"></script>
+    <script src="https://unpkg.com/ionicons@4.2.2/dist/ionicons.js"></script>
+	
 </body>
+
 </html>
