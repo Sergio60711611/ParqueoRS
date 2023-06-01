4c0407e5251acc3b4412cce0c1477635cc3790ef
sergio60711611 <Sergio_01_gabriel@hotmail.com>
2023-06-01 17:46:06 -0400

    loginterminado

--- resources/views/inicio/login.blade.php
+++ resources/views/inicio/login.blade.php
@@ -14,50 +14,44 @@
 
 <body>
 
-    <div class="container" id="container">
+<div class="container" id="container">
         <div class="form-container sign-up-container">
-					
-<form id="msform">
-  <!-- progressbar -->
-  <ul id="progressbar">
-    <li class="active">Account Setup</li>
-    <li>Social Profiles</li>
-    <li>Personal Details</li>
+            <form action="#">
+            <ul id="progressbar">
+    <li class="active">Paso N</li>
+    <li>Paso N</li>
+    
   </ul>
-  <!-- fieldsets -->
-  <fieldset>
-    <h2 class="fs-title">Create your account</h2>
-    <h3 class="fs-subtitle">This is step 1</h3>
-    <input type="text" name="email" placeholder="Email" />
-    <input type="password" name="pass" placeholder="Password" />
-    <input type="password" name="cpass" placeholder="Confirm Password" />
-    <input type="button" name="next" class="next action-button" value="Next" />
-  </fieldset>
-  <fieldset>
-    <h2 class="fs-title">Social Profiles</h2>
-    <h3 class="fs-subtitle">Your presence on the social network</h3>
-    <input type="text" name="twitter" placeholder="Twitter" />
-    <input type="text" name="facebook" placeholder="Facebook" />
-    <input type="text" name="gplus" placeholder="Google Plus" />
-    <input type="button" name="previous" class="previous action-button" value="Previous" />
-    <input type="button" name="next" class="next action-button" value="Next" />
-  </fieldset>
-  <fieldset>
-    <h2 class="fs-title">Personal Details</h2>
-    <h3 class="fs-subtitle">We will never sell it</h3>
-    <input type="text" name="fname" placeholder="First Name" />
-    <input type="text" name="lname" placeholder="Last Name" />
-    <input type="text" name="phone" placeholder="Phone" />
-    <textarea name="address" placeholder="Address"></textarea>
-    <input type="button" name="previous" class="previous action-button" value="Previous" />
-    <a href="https://twitter.com/GoktepeAtakan" class="submit action-button" target="_top">Submit</a>
-  </fieldset>
-</form>
+                <fieldset>
+                    <h1>Datos Cliente</h1>
+                    
+                    <input type="text" placeholder="Nombre">
+                    <input type="text" placeholder="Apellido">
+                    <input type="text" placeholder="Celular">
+                    <input type="text" placeholder="CI">
+                    <input type="email" placeholder="Correo">
+                    <input type="password" placeholder="Contraseña">
+                    <input type="button" name="next" class="next action-button" value="Suigiente" style="border-radius: 20px; border: 1px solid #ff4b2b; background: #ff4b2b; color: #fff; font-size: 12px; font-weight: bold; padding: 12px 45px; letter-spacing: 1px; text-transform: uppercase; transition: transform 80ms ease-in;" />
+
+                </fieldset>
+                <fieldset>
+                
+                    <h1>Datos Vehiculo</h1>
+                    
+                    <input type="text" placeholder="Marca">
+                    <input type="text" placeholder="Modelo">
+                    <input type="text" placeholder="Placa">
+                    <input type="text" placeholder="Color">
+                    <button type="submit" class="submit-button" style="border-radius: 20px; border: 1px solid #ff4b2b; background: #ff4b2b; color: #fff; font-size: 12px; font-weight: bold; padding: 12px 45px; letter-spacing: 1px; text-transform: uppercase; transition: transform 80ms ease-in;">Enviar</button>
+                    <input type="button" name="previous" class="previous action-button" value="Anterior" style="border-radius: 20px; border: 1px solid #ff4b2b; background: #ff4b2b; color: #fff; font-size: 12px; font-weight: bold; padding: 12px 45px; letter-spacing: 1px; text-transform: uppercase; transition: transform 80ms ease-in;" />
+                </fieldset>
+                
+            </form>
             
         </div>
         <div class="form-container sign-in-container">
             <form action="#">
-                <h1>Sign In</h1>
+                <h1>Iniciar Sesion</h1>
                 <div class="social-container">
                     <a href="usuario" class="social" title="Usuario">
 						<ion-icon name="person-outline"></ion-icon>
@@ -69,24 +63,24 @@
 						<ion-icon name="person"></ion-icon>
                     </a>
                 </div>
-                <span>or use your account</span>
-                <input type="email" placeholder="Email">
-                <input type="password" placeholder="Password">
-                <button>Sign In</button>
+                <span>Seleccion tu tipo de usuario</span>
+                <input type="email" placeholder="Correo">
+                <input type="password" placeholder="Contraseña">
+                <button>Iniciar</button>
             </form>
         </div>
         <div class="overlay-container">
             <div class="overlay">
                 <div class="overlay-panel overlay-left">
-                    <h1>Welcome Back</h1>
-                    <p>To keep connected with us please login with your personal info</p>
-                    <button class="ghost" id="signIn">Sign In</button>
+                    <h1>Bienvenido</h1>
+                    <p>Para mantenerse conectado con nosotros, inicie sesión con su información personal</p>
+                    <button class="ghost" id="signIn">Iniciar Sesion</button>
 					
                 </div>
                 <div class="overlay-panel overlay-right">
-                    <h1>Hello,Friend</h1>
-                    <p>Enter your personal details and start journey with us</p>
-                    <button class="ghost" id="signUp">Sign Up</button>
+                    <h1>Eres Nuevo?</h1>
+                    <p>Ingresa tus datos personales y comienza tu viaje con nosotros</p>
+                    <button class="ghost" id="signUp">Registrarse</button>
                 </div>
             </div>
         </div>
@@ -95,7 +89,10 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
     <script src="\js\login.js"></script>
     <script src="https://unpkg.com/ionicons@4.2.2/dist/ionicons.js"></script>
-	
+    
+	<button class="btn-corner" onclick="window.location.href = '..'">Atras</button>
+
+
 </body>
 
 </html>
