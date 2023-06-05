e09d4ae032540ed2254ba1edf4d663253924349e
sergio60711611 <Sergio_01_gabriel@hotmail.com>
2023-06-05 06:10:24 -0400

    ayhuda arreglado

--- resources/views/ayuda.blade.php
+++ resources/views/ayuda.blade.php
@@ -181,7 +181,7 @@
     }</style>
     <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-    <script src="{{ asset('js/ayuda.js') }}"></script>
+    <script src=".\js\ayuda.js"></script>
     
 </body>
 </html>
