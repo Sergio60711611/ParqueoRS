451534c9b85caf6ea67bd7326a0d0ef61fcc94f5
QTMChris <202000653@est.umss.edu>
2023-06-12 06:24:14 -0400

    "nav"

--- resources/views/administrador/navbar.blade.php
+++ resources/views/administrador/navbar.blade.php
@@ -151,6 +151,14 @@
                                 </a>
                             </li>
                             </ul>
+                            <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="/administrador/reportess" class="nav-link active" style="background-color: #395261; color:#FFFFFF">
+                                <i class="nav-icon fas fa-users"></i>
+                                <p>Ingresos</p>
+                                </a>
+                            </li>
+                            </ul>
                         </li>
                         <li class="nav-item">
                             <a href="/" class="nav-link active" style="background-color: #FD968E; color:#324855; margin-top: 3px;">
