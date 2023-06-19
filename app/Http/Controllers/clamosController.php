c6c47498fd31e20ed35d702bac717f6e62f695aa
QTMChris <202000653@est.umss.edu>
2023-06-19 07:31:08 -0400

    recla

--- app/Http/Controllers/clamosController.php
+++ app/Http/Controllers/clamosController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\cliente;
+use App\Models\Cliente;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\ReclamoSugerenciaMail;
 
